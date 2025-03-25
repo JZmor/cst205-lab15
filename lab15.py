7ded69d5d8a971bf8b2038a25b7d5b247e59f38e
Jacob Zamora <113962665+JZmor@users.noreply.github.com>
2025-03-25 07:37:05 -0700

    first commit

---
+++ lab15.py
@@ -0,0 +1,18 @@
+# hello_flask.py
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+
+# create an instance of Flask
+app = Flask(__name__)
+
+# route decorator binds a function to a URL
+@app.route('/')
+def hello():
+  return '<p>Thomas : afk<p><p>Jacob : who<p>'
+
+@app.route('/jacob')
+def t_test():
+   return render_template('template.html')
+
+bootstrap = Bootstrap5(app)
